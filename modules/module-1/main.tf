ca73dffe508c40ea8ad8ea95ddce1c49b9725813
goveri1165 <89534194+goveri1165@users.noreply.github.com>
2022-11-30 19:20:20 +0530

    Update main.tf

--- modules/module-1/main.tf
+++ modules/module-1/main.tf
@@ -3200,6 +3200,11 @@ resource "aws_s3_bucket" "bucket_upload" {
   }
 }
 
+resource "aws_s3_bucket_server_side_encryption_configuration" "example" {  bucket = aws_s3_bucket.bucket_upload.bucket
+
+  rule {    apply_server_side_encryption_by_default {     
+      sse_algorithm     = "AES256"    }}
+}
 
 resource "aws_s3_bucket_policy" "allow_access_for_prod" {
   bucket = aws_s3_bucket.bucket_upload.id
